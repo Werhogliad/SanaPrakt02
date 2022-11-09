3224760ab59a7491e71f1b898fca7ee8a9d6aebd
Verkhohliad Denys <sana22_vdv@student.ztu.edu.ua>
2022-11-09 21:07:42 +0200

    Created dublicates

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,3 +4,7 @@ using System.Text;
 Console.OutputEncoding = Encoding.UTF8;
 
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
